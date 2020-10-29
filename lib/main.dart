8ed9ce2dc5708458b4adebf5aec7a52ce177f08e
BrunoMCerqueira <brunocerqueirasd@gmail.com>
2020-10-29 15:28:26 -0300

    Add device android. Adjust main page to show layout as in the course.

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,36 @@ void main() {
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                color: Colors.red,
+                width: 100.0,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 100.0,
+                    height: 100.0,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    width: 100.0,
+                    height: 100.0,
+                    color: Colors.green,
+                  )
+                ],
+              ),
+              Container(
+                color: Colors.blue,
+                width: 100.0,
+              )
+            ],
+          ),
+        ),
       ),
     ),
   );
